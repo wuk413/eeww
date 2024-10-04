409a8089c1e8146cbd60bac4fff6e0e13a7deaa9
wuk413 <ryu29062003@gmail.com>
2024-10-04 12:20:03 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import socket
+from datetime import datetime
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    pod_ip = request.remote_addr
+    pod_name = socket.gethostname()
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    uri = request.url
+    return {
+        "Pod IP": pod_ip,
+        "Pod Name": pod_name,
+        "Current Time": current_time,
+        "URI": uri
+    }
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
